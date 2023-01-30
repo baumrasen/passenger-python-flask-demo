9f2bc7dbf596cc91ede98ea0660d11aaeadb3ff5
baumrasen <baumrasen@gmail.com>
2023-01-30 15:20:29 +0100

    added .venv to path

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ The `master` branch contains the code without Passenger installed.
 The `end_result` branch contains the code with Passenger installed.
 
 Run `git diff origin/master..origin/end_result` to see what's different.
+
+## Howto setup on webhosting
+### first: on your develop maschine
+- get the correct python version
+- make a venv with this version
+- install packages
+
+        pip install -r requirements.txt
+
+- copy the files from the venv folder to the working directory on the webhosting
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,16 @@
+import sys, os
+cwd = os.getcwd()
+sys.path.append(cwd)
+
+#project_location = cwd + '/myApp'
+project_location = cwd
+venv_location = cwd + '/.venv'
+print (project_location)
+print (venv_location)
+sys.path.insert(0, project_location)
+
+#sys.path.insert(0,'/home/myuser/mydomain.com/env/bin')
+#sys.path.insert(0,'/home/myuser/mydomain.com/env/lib/python2.7/site-packages/django')
+#sys.path.insert(0,'/home/myuser/mydomain.com/env/lib/python2.7/site-packages')
+
 from app import MyApp as application
