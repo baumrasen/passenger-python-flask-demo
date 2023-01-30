c31750768df3af65efd714f1d583feeb8d6c7e78
baumrasen <baumrasen@gmail.com>
2023-01-30 15:41:24 +0100

    updated path settings

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,11 +5,14 @@ sys.path.append(cwd)
 #project_location = cwd + '/myApp'
 project_location = cwd
 venv_location = cwd + '/.venv'
+site_packages = venv_location + '/Lib/site-packages'
 print (project_location)
 print (venv_location)
-sys.path.insert(0, project_location)
+print (site_packages)
+#sys.path.insert(0, project_location)
+sys.path.insert(0, site_packages)
 
-#sys.path.insert(0,'/home/myuser/mydomain.com/env/bin')
+# sys.path.insert(0,'/home/myuser/mydomain.com/env/bin')
 #sys.path.insert(0,'/home/myuser/mydomain.com/env/lib/python2.7/site-packages/django')
 #sys.path.insert(0,'/home/myuser/mydomain.com/env/lib/python2.7/site-packages')
 
